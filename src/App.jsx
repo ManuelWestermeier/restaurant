15f469cdeaf15449f75c49926971d13f2945e525
manuelwestermeier <westermeier111@gmail.com>
2026-03-03 19:47:54 +0100

    update to test-website

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Route path="/" />
+      <Route path="/impressum" />
+      <Route path="/datenschutz" />
+      <Route path="/" />
+      <Route path="*" />
+    </BrowserRouter>
+  );
 }
 export default App;
